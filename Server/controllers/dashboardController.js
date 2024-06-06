47d49ab43bfe97a8d6912c15f68eca84ac30b729
Aung Kaung Myat <rapskill095@gmail.com>
2024-06-06 14:42:26 +0630

    successfully separated owner's own workers.

--- Server/controllers/dashboardController.js
+++ Server/controllers/dashboardController.js
@@ -30,21 +30,22 @@ export const retrieveDataForDashboard=asyncHandler(async(req,res)=>{
       res.status(500).send('An error occurred');
     });
   })
-
+//-------------------------------------------------------------------------------
   export const retrieveWorkerDataForOwner=asyncHandler(async(req,res)=>{
     const tableData={
       worker:""
     }
-    const sqlWorker="select * from worker";
-    const queryDatabase=(sql)=>{
+    const sqlWorker="select * from worker where WorkerId in (select WorkerId from worker_detail where UserId=?)";
+    const UserId=req.user.id;
+    const queryDatabase=(sql,value)=>{
       return new Promise((resolve,reject)=>{
-        connection.query(sql,(err,result)=>{
+        connection.query(sql,value,(err,result)=>{
           if(err) return reject(err);
           resolve (result);
         })
       })
     }
-    Promise.all([queryDatabase(sqlWorker)])
+    Promise.all([queryDatabase(sqlWorker,UserId)])
     .then(([workerResult])=>{
       //console.log(workerResult)
       tableData.worker = workerResult;
@@ -55,27 +56,50 @@ export const retrieveDataForDashboard=asyncHandler(async(req,res)=>{
       res.status(500).send('An error occurred');
     });
   })
-
+//---------------------------------------------------------------------
   export const createWorker=asyncHandler(async(req,res)=>{
+    const detail={
+      worker:"",
+      worker_detail:"",
+    }
     const isoDate = new Date();
   const mySQLDateString = isoDate.toJSON().slice(0, 19).replace('T', ' ');
   const {name,gender,phone,address,age}=req.body;
+  const UserId=req.user.id;
+  console.log('Owner id',UserId)
 
-  const sql='insert into worker(Name,Gender,Phone_no,Address,Age,Created_at) values(?,?,?,?,?,?)';
-  //const insertToWorker_Detail='insert into worker_detail(UserId,WorkerId) values(?,?)';
-  const values=[name,gender,phone,address,age,mySQLDateString];
-    
-  connection.query(sql,values, function(err, result) {
-    if (err) {
-      console.error("Error inserting data:", err);
-      res.status(500).json({ error: "An error occurred while inserting data" });
-    } else {
-      console.log("Data inserted successfully!");
-      res.json({ message: "Data inserted successfully" });
+  const retrieveId='select WorkerId from worker where Phone_no = ?';
+  const insertToWorker='insert into worker(Name,Gender,Phone_no,Address,Age,Created_at) values(?,?,?,?,?,?)';
+  const insertToWorker_Detail='insert into worker_detail(UserId,WorkerId) values(?,?)';
+  const valueForWorker=[name,gender,phone,address,age,mySQLDateString];
+  
+  const phoneNo=[phone];
+
+  //function to query database
+  const queryDatabase=(sql,value)=>{
+    return new Promise((resolve,reject)=>{
+      connection.query(sql,value,(error,result)=>{
+        if(error) return reject(error);
+        resolve(result);
+      })
+    })
+  }
+
+  const resolvePromise=Promise.resolve(queryDatabase(insertToWorker,valueForWorker))
+  resolvePromise.then(async(value)=>{
+    console.log("Id of worker : ",value.insertId)
+    const vlaueForWorker_Detail=[UserId,value.insertId];
+    const queryResult=await queryDatabase(insertToWorker_Detail,vlaueForWorker_Detail)
+    if(queryResult){
+      console.log(" success.")
     }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('An error occurred');
   });
   })
-
+//--------------------------------------------------------------------
   export const deleteUser=asyncHandler(async(req,res)=>{
     console.log("User Id to delete",req.body)
     const {id}=req.body;
@@ -93,7 +117,7 @@ export const retrieveDataForDashboard=asyncHandler(async(req,res)=>{
       }
     })
   })
-
+//-----------------------------------------------------------------
   export const deleteWorker=asyncHandler(async(req,res)=>{
     //console.log("Worker Id to delete",req.body)
     const {id}=req.body; 
